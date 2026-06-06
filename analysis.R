7f286b667962df0169fe1b72143f9911a509845e
dshah0567 <dsha0567@uni.sydney.edu.au>
2026-06-06 20:47:40 +1000

    Create analysis.R

---
+++ analysis.R
@@ -0,0 +1,11 @@
+# Transport Accessibility in Greater Western Sydney
+
+print("Project started")
+
+research_questions <- c(
+  "Which suburbs have the best access to public transport?",
+  "How does accessibility vary across Greater Western Sydney?",
+  "How might transport access affect education and employment?"
+)
+
+print(research_questions)
